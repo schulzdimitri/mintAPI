89545f1d3acc6cdfc6bc4cde327efedd08917483
Dimitri <dimitri.s.amado@gmail.com>
2026-03-04 00:20:22 -0300

    feat: Implement UserRegister with tests

---
+++ src/controllers/user_register_test.py
@@ -0,0 +1,26 @@
+from .user_register import UserRegister
+
+
+class MockUserRepository():
+    def __init__(self) -> None:
+        self.registry_user_attributes = {}
+    
+    def registry_user(self, username: str, password: str) -> None:
+        self.registry_user_attributes["username"] = username
+        self.registry_user_attributes["password"] = password
+
+def test_registry():
+    repository = MockUserRepository()
+    controller = UserRegister(repository)
+    
+    username = "test_user"
+    password = "test_password"
+    
+    response = controller.registry(username, "test_password")
+    
+    assert response["type"] == "User"
+    assert response["username"] == username 
+    
+    assert repository.registry_user_attributes["username"] == username
+    assert repository.registry_user_attributes["password"] is not None
+    assert repository.registry_user_attributes["password"] != password
