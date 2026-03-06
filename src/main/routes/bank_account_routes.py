cd340d050e430b477fe500fc917ebd9b4325f624
Dimitri <dimitri.s.amado@gmail.com>
2026-03-06 13:44:06 -0300

    feat: Implement route with controllers logic

--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -1,11 +1,26 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+
+from src.main.composer.balance_editor_composer import balance_editor_composer
+from src.main.composer.login_creator_composer import login_creator_composer
+from src.main.composer.user_register_composer import user_register_composer
+from src.views.http_types.http_request import HttpRequest
 
 bank_account_bp = Blueprint('bank_routes', __name__)
 
-@bank_account_bp.route('/', methods=['GET'])
-def get_bank_accounts():
-    bank_accounts = [
-        {"id": 1, "name": "Checking Account", "balance": 1500.00},
-        {"id": 2, "name": "Savings Account", "balance": 5000.00}
-    ]
-    return jsonify(bank_accounts, 200)
+@bank_account_bp.route('/bank/registry', methods=['POST'])
+def registry_user():
+    http_request = HttpRequest(body=request.json)
+    http_response = user_register_composer().handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+@bank_account_bp.route('/bank/login', methods=['POST'])
+def login():
+    http_request = HttpRequest(body=request.json)
+    http_response = login_creator_composer().handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
+
+@bank_account_bp.route('/bank/balance/<user_id>', methods=['PATCH'])
+def edit_balance(user_id):
+    http_request = HttpRequest(body=request.json, params={'user_id': user_id})
+    http_response = balance_editor_composer().handle(http_request)
+    return jsonify(http_response.body), http_response.status_code
