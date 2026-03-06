ca6baec20b7c13f3a2af1fdb521bc77c0abee5d8
Dimitri <dimitri.s.amado@gmail.com>
2026-03-06 11:35:24 -0300

    feat: Implement BalanceEditorView and LoginCreatorView

---
+++ src/views/balance_editor_view.py
@@ -0,0 +1,25 @@
+from controllers.interfaces.balance_editor import BalanceEditorInterface
+
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interfaces import ViewInterface
+
+
+class BalanceEditorView(ViewInterface):
+    def __init__(self, controller: BalanceEditorInterface) -> None:
+        self.__controller = controller
+        
+    def handle(self, http_request):
+        new_balance = http_request.body.get('new_balance')
+        user_id = http_request.body.param.get('user_id')
+        self.__validate_inputs(new_balance, user_id)
+        
+        response = self.__controller.edit_balance(new_balance, user_id)
+        return HttpResponse(body={ 'data': response }, status_code=200)
+        
+    def __validate_inputs(self, new_balance: any, user_id: any) -> None:
+        if (
+            not new_balance
+            or not user_id
+            or not isinstance(new_balance, float)
+        ): raise ValueError('Invalid input data')
---
+++ src/views/login_creator_view.py
@@ -0,0 +1,27 @@
+from src.controllers.interfaces.login_creator import LoginCreatorInterface
+
+from .http_types.http_request import HttpRequest
+from .http_types.http_response import HttpResponse
+from .interfaces.view_interfaces import ViewInterface
+
+
+class LoginCreatorView(ViewInterface):
+    def __init__(self, controller: LoginCreatorInterface) -> None:
+        self.__controller = controller
+        
+    def handle(self, http_request: HttpRequest) -> HttpResponse:
+        username = http_request.body.get('username')
+        password = http_request.body.get('password')
+        self.__validate_input(username, password)
+        
+        response = self.__controller.create(username, password)
+        return HttpResponse(body={ 'data': response }, status_code=200)
+        
+    def __validate_input(self, username: any, password: any) -> None:
+        if (
+            not username
+            or not password
+            or not isinstance(username, str)
+            or not isinstance(password, str)
+        ):
+            raise ValueError('Invalid input')
