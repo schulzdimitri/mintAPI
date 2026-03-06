bc53bbeb141deae7f1cd2231f0295ab71357c9a6
Dimitri <dimitri.s.amado@gmail.com>
2026-03-06 15:38:53 -0300

    feat: Implement middleware with auth_verify

---
+++ src/main/middlewares/auth_jwt.py
@@ -0,0 +1,23 @@
+import jwt
+from flask import request
+
+from src.drivers.jwt_handler import JWTHandler
+
+
+def auth_jwt_verify():
+    jwt_handler = JWTHandler()
+    raw_token = request.headers.get('Authorization')
+    user_id = request.headers.get("uid")
+    
+    if not raw_token or not user_id:
+        raise Exception("Invalid Auth informations")
+    
+    token = raw_token.split()[1]
+    token_info = jwt_handler.decode_jwt_token(token)
+    token_uid = token_info["user_id"]
+    
+    if token_uid and user_id and (int(token_uid) == int(user_id)):
+        return token_info
+    
+    raise Exception("User Unauthorized")
+    
--- src/main/routes/bank_account_routes.py
+++ src/main/routes/bank_account_routes.py
@@ -3,6 +3,7 @@ from flask import Blueprint, jsonify, request
 from src.main.composer.balance_editor_composer import balance_editor_composer
 from src.main.composer.login_creator_composer import login_creator_composer
 from src.main.composer.user_register_composer import user_register_composer
+from src.main.middlewares.auth_jwt import auth_jwt_verify
 from src.views.http_types.http_request import HttpRequest
 
 bank_account_bp = Blueprint('bank_routes', __name__)
@@ -21,6 +22,12 @@ def login():
 
 @bank_account_bp.route('/bank/balance/<user_id>', methods=['PATCH'])
 def edit_balance(user_id):
-    http_request = HttpRequest(body=request.json, params={'user_id': user_id})
+    token_info = auth_jwt_verify()
+    http_request = HttpRequest(
+        body=request.json, 
+        params={'user_id': user_id},
+        token_infos=token_info,
+        headers=request.headers
+    )
     http_response = balance_editor_composer().handle(http_request)
     return jsonify(http_response.body), http_response.status_code
